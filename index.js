7155672b57fb3c7afd4ea20d1d9de026ecdbd68e
Mohit Sharma <mohitsharma23j97@gmail.com>
2024-09-19 23:12:17 +0530

    feat: initialized commander options

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 #! /usr/bin/env node
 
-console.log("Started Forge");
+const fs = require("fs");
+const path = require("path");
+const { program } = require("commander");
+
+program.version("1.0.0").description("Forge - A CLI scaffold tool");
+
+program.option("-db, --database <database>", "Add database");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.database === "mongo") {
+  const folderPath = path.join(process.cwd(), "config");
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+    console.log("Folder created success");
+  }
+}
+
+program.parse(process.argv);
