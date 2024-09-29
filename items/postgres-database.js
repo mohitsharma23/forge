b3065460b63da5f42884c2ac1aee6b52cd0983a0
Mohit Sharma <mohitsharma23j97@gmail.com>
2024-09-29 10:12:46 +0530

    feat: updated postgress connection file

--- items/postgres-database.js
+++ items/postgres-database.js
@@ -1,1 +1,14 @@
-// Handle postgres connection
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
+
+export default pool;
